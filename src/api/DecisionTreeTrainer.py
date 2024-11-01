bad2553b6c02f6f688fd4ae64aeb14b75e6d2abd
NianwenDan <dannianwen@gmail.com>
2024-11-01 13:47:03 -0700

    Built basic API structure and some training methods (#1)
    * feat: added test data to train decision tree and generate example outout
    
    * feat: support cookie to identify user
    
    * feat: finalized api structure
    
    * feat: support output decision tree image
    
    * feat: require token to protect the server
    
    * docs: update .gitignore
    
    * style: renamed decisionTree.py to DecisionTreeTrainer.py

---
+++ src/api/DecisionTreeTrainer.py
@@ -0,0 +1,139 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import _tree
+from sklearn import tree
+from sklearn import metrics
+import matplotlib.pyplot as plt
+import time
+from io import BytesIO
+
+class DecisionTreeTrainer:
+    def __init__(self, dataset_path: str):
+        self._status = {
+            'code' : -1,
+            'msg' : 'NOT TRAINED'
+        }
+        self._df = pd.read_csv(dataset_path)
+        self._X_train = None
+        self._X_test = None
+        self._y_train = None
+        self._y_test = None
+        self._clf = None
+
+        self._predict_data = None
+
+    def status(self):
+        # print(self.status)
+        return self._status
+
+    def train(self, target_column_name: str, test_size: float) -> None:
+        # update status
+        self._status['code'] = 1
+        self._status['msg'] = 'TRAINING IN PROGRESS'
+        try:
+            # features (all coloums except target)
+            X = self._df.drop(target_column_name, axis=1)
+            # target coloum/variable
+            y = self._df[target_column_name]
+            # Split dataset into training set and test set
+            self._X_train, self._X_test, self._y_train, self._y_test = train_test_split(X, y, test_size=test_size, random_state=1)
+            # create a decision tree classifier
+            self._clf = DecisionTreeClassifier(
+                criterion='entropy', 
+                random_state=100, 
+                max_depth=3, 
+                min_samples_split=5
+            )
+            # train and fit decision tree classifer
+            self._clf = self._clf.fit(self._X_train, self._y_train)
+        except Exception as e:
+            self._status['code'] = 2
+            self._status['msg'] = f'AN ERROR OCCURRED DURING THE TRAINING: {e}'
+        
+        # time.sleep(10000) # Async Testing
+        
+        self._status['code'] = 0
+        self._status['msg'] = 'TRAINING COMPLETED'
+
+    def predict(self):
+        # train not started/finished
+        if self._status['code'] != 0:
+            return None
+        # no prediction yet, calculate one
+        if not self._predict_data:
+            dtree_y_pred = self._clf.predict(self._X_test)
+            accuracy = metrics.accuracy_score(self._y_test, dtree_y_pred)
+            precision = metrics.precision_score(self._y_test, dtree_y_pred, average='weighted')
+            recall = metrics.recall_score(self._y_test, dtree_y_pred, average='weighted')
+            f1_score = metrics.f1_score(self._y_test, dtree_y_pred, average='weighted')
+            # update predition data
+            self._predict_data = {
+                'accuracy': accuracy,
+                'precision': precision,
+                'recall': recall,
+                'f1_score': f1_score
+            }        
+        return self._predict_data
+
+    def tree_structure(self):
+        # train not started/finished
+        if self._status['code'] != 0:
+            return None
+        tree_ = self._clf.tree_
+        feature_names = self._clf.feature_names_in_
+
+        def dfs(node):
+            # get necessary value
+            feature_name = feature_names[tree_.feature[node]]
+            threshold = float(tree_.threshold[node])
+            training_samples_reached = int(tree_.n_node_samples[node])
+            value = tree_.value[node].tolist()
+            impurity = float(tree_.impurity[node])
+            res = {
+                "data": {
+                    "feature": feature_name,
+                    "threshold": threshold,
+                    "training_samples_reached": training_samples_reached,
+                    "value": value,
+                    "impurity": impurity
+                },
+                "left": None,
+                "right": None
+            }
+            
+            # base case: If this is a leaf node
+            if tree_.feature[node] == _tree.TREE_UNDEFINED:
+                return res 
+            # recursive case: Not a leaf node
+            # create the node dictionary with feature, threshold, and recursive children
+            res['left'] = dfs(tree_.children_left[node])
+            res['right'] = dfs(tree_.children_right[node])
+            return res
+        
+        # Start recursion from the root (node 0)
+        return dfs(0)
+    
+    def tree_img(self, length, width, dpi):
+        # train not started/finished
+        if self._status['code'] != 0:
+            return None
+        feature_names = self._clf.feature_names_in_
+        if not length:
+            length = 12
+        if not width:
+            width = 8
+        if not dpi:
+            dpi = 150
+
+        img_io = BytesIO() # create the in-memory bytes buffer
+        # plot the decision tree
+        plt.figure(figsize=(length, width))
+        tree.plot_tree(self._clf, filled=True, feature_names=feature_names)
+        # save the plot to the in-memory buffer, not to a file
+        plt.savefig(img_io, format='png', dpi=dpi)
+        plt.close()  # close the plot to free memory
+        # move the cursor of the BytesIO object to the start
+        img_io.seek(0)
+        return img_io
+
---
+++ src/api/main.py
@@ -0,0 +1,158 @@
+from flask import Flask, request, jsonify, make_response, send_file
+from api.DecisionTreeTrainer import decisionTree
+import threading
+import uuid
+from functools import wraps
+from config import SECRET_TOKEN
+
+app = Flask(__name__)
+
+# @app.route('/')
+# def index():
+#     return redirect("https://google.com")
+
+db = {}
+
+def token_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        token = request.headers.get("Authorization")  # get the token from the header
+        # check if token exists and matches the secret token
+        if not token or token.split(" ")[1] != SECRET_TOKEN:
+            return jsonify({'code' : 401, "msg": "INVALID OR MISSING TOKEN"}), 401
+
+        return f(*args, **kwargs)
+    return decorated_function
+
+def is_user_exists(request):
+    uuid4 = request.cookies.get('uuid')
+    if uuid4 and uuid4 in db:
+        return True
+    return False
+
+def get_user_model(request):
+    uuid4 = request.cookies.get('uuid')
+    return db[uuid4]['model']
+
+@app.errorhandler(404)
+@token_required
+def page_not_found(error):
+    return make_response(jsonify({'code' : 404, 'msg' : 'API NOT FOUND'}), 404)
+
+@app.route('/api/v1/system/status', methods=['GET'])
+@token_required
+def get_system_status():
+    if not is_user_exists(request):
+        response = make_response(jsonify({'code' : 200, 'userId': None, 'msg' : 'SYSTEM NORMAL'}), 200)
+        response.set_cookie('uuid', '', max_age=0) # expire the cookie now
+        return response
+    uuid4 = request.cookies.get('uuid')
+    return make_response(jsonify({'code' : 200, 'userId': uuid4, 'msg' : 'SYSTEM NORMAL'}), 200)
+
+@app.route('/api/v1/system/set-userId', methods=['GET'])
+@token_required
+def set_userId():
+    # Set a cookie in the response object
+    uuid4 = str(uuid.uuid4())
+    db[uuid4] = {'id' : uuid4,
+                 'model' : decisionTree(dataset_path='./diabetes.csv')
+                }
+    response = make_response(jsonify({'code' : 200, 'userId': uuid4, 'msg' : 'NEW USER CREATED'}), 200) # create a response object
+    response.set_cookie('uuid', uuid4, max_age=60*60*2)  # cookie valid for 2 hours
+    return response
+
+@app.route('/api/v1/system/get-userId', methods=['GET'])
+@token_required
+def get_userId():
+    if is_user_exists(request):
+        return make_response(jsonify({'code' : 200, 'userId': request.cookies.get('uuid'), 'msg' : 'OK'}), 200)
+    else:
+        response = make_response(jsonify({'code' : 404, 'userId': None, 'msg' : 'NO USER FOUND'}), 404)
+        response.set_cookie('uuid', '', max_age=0) # expire the cookie now
+        return response
+
+@app.route('/api/v1/model/train-status', methods=['GET'])
+@token_required
+def get_model_status():
+    if not is_user_exists(request):
+        return make_response(jsonify({'code' : 404, 'userId': None, 'msg' : 'NO USER FOUND, CREATE A USER FIRST'}), 404)
+    model = get_user_model(request)
+    data = {
+        'code' : 200,
+        'userId': request.cookies.get('uuid'),
+        'msg' : 'OK',
+        'data': model.status()
+    }
+    return make_response(jsonify(data), data['code'])
+
+@app.route('/api/v1/model/train-start', methods=['GET'])
+@token_required
+def get_model_train():
+    if not is_user_exists(request):
+        return make_response(jsonify({'code' : 404, 'userId': None, 'msg' : 'NO USER FOUND, CREATE A USER FIRST'}), 404)
+    model = get_user_model(request)
+    # Start the training in a separate thread
+    threading.Thread(model.train(target_column_name='Outcome', test_size=0.2)).start()
+    data = {
+        'code' : 200,
+        'userId': request.cookies.get('uuid'),
+        'msg' : 'TRAINING START, USE /train-status TO CHECK CURRENT TRAINING STATUS'
+    }
+    return make_response(jsonify(data), data['code'])
+
+@app.route('/api/v1/model/predict', methods=['GET'])
+@token_required
+def get_model_predict():
+    if not is_user_exists(request):
+        return make_response(jsonify({'code' : 404, 'userId': None, 'msg' : 'NO USER FOUND, CREATE A USER FIRST'}), 404)
+    model = get_user_model(request)
+    predict = model.predict()
+    data = {
+        'code' : 200,
+        'userId': request.cookies.get('uuid'),
+        'msg' : 'OK',
+        'data': predict
+    }
+    if not predict:
+        data['code'] = 404
+        data['msg'] = 'TRAINING PROCEDURE NOT STARTED OR INCOMPLETE, USE /train-status TO CHECK CURRENT TRAINING STATUS'
+    return make_response(jsonify(data), data['code'])
+
+@app.route('/api/v1/tree/structure', methods=['GET'])
+@token_required
+def get_tree_structure():
+    if not is_user_exists(request):
+        return make_response(jsonify({'code' : 404, 'userId': None, 'msg' : 'NO USER FOUND, CREATE A USER FIRST'}), 404)
+    model = get_user_model(request)
+    data = {
+        'code' : 200,
+        'userId': request.cookies.get('uuid'),
+        'msg' : 'OK',
+        'data': model.tree_structure()
+    }
+    return make_response(jsonify(data), data['code'])
+
+@app.route('/api/v1/tree/image', methods=['GET'])
+@token_required
+def get_tree_image():
+    if not is_user_exists(request):
+        return make_response(jsonify({'code' : 404, 'userId': None, 'msg' : 'NO USER FOUND, CREATE A USER FIRST'}), 404)
+    length = request.args.get('length', type = int)
+    width = request.args.get('width', type = int)
+    dpi = request.args.get('dpi', type = int)
+    model = get_user_model(request)
+    img = model.tree_img(length, width, dpi)
+    if not img:
+        data = {
+            'code' : 404,
+            'userId': request.cookies.get('uuid'),
+            'msg' : 'TRAINING PROCEDURE NOT STARTED OR INCOMPLETE, USE /train-status TO CHECK CURRENT TRAINING STATUS',
+            'data': img
+        }
+        return make_response(jsonify(data), data['code'])
+    return send_file(img, mimetype='image/png')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5500, debug=True)
+    # app.run(host='0.0.0.0', port=5500)
+    # print(users)

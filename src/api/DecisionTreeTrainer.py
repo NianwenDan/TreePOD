188d6eb235d58730ef8b29134fac3ddc4ac4a5c3
NianwenDan <dannianwen@gmail.com>
2024-11-01 18:20:06 -0700

    feat: merged Zhuo/QuantitativePlots  (#2)
    * Sampling candidate trees
    
    * Example output json
    
    * docs: added requirements.txt
    
    * refactor: merged decisionTreeTrainer
    
    * refactor: merged decisionTreeCandidateGenerator
    
    * feat: add feature to output single tree structure/image
    
    * refactor: merged DecisionTreeConfig
    
    * refactor: integrated all features to the main.py
    
    * refactor: remove merged createCandidates.py
    
    ---------
    
    Co-authored-by: Yihe Zhuo <yihe.ruby.zhuo@gmail.com>

--- src/api/DecisionTreeTrainer.py
+++ src/api/DecisionTreeTrainer.py
@@ -8,63 +8,48 @@ import matplotlib.pyplot as plt
 import time
 from io import BytesIO
 
-class DecisionTreeTrainer:
-    def __init__(self, dataset_path: str):
-        self._status = {
-            'code' : -1,
-            'msg' : 'NOT TRAINED'
-        }
-        self._df = pd.read_csv(dataset_path)
-        self._X_train = None
-        self._X_test = None
-        self._y_train = None
-        self._y_test = None
-        self._clf = None
+class decisionTreeTrainer:
+    def __init__(self, tree_id, X_train, y_train, X_test, y_test, feature_subset=None):
+        self._tree_id = tree_id
+        self._X_train = X_train
+        self._X_test = X_test
+        self._y_train = y_train
+        self._y_test = y_test
+        self._tree = None
+        # Only use feature_subset
+        if feature_subset:
+            self._X_train = self._X_train.iloc[:, feature_subset]
+            self._X_test = self._X_test.iloc[:, feature_subset]
 
         self._predict_data = None
 
-    def status(self):
-        # print(self.status)
-        return self._status
+    def id(self):
+        return self._tree_id
 
-    def train(self, target_column_name: str, test_size: float) -> None:
-        # update status
-        self._status['code'] = 1
-        self._status['msg'] = 'TRAINING IN PROGRESS'
-        try:
-            # features (all coloums except target)
-            X = self._df.drop(target_column_name, axis=1)
-            # target coloum/variable
-            y = self._df[target_column_name]
-            # Split dataset into training set and test set
-            self._X_train, self._X_test, self._y_train, self._y_test = train_test_split(X, y, test_size=test_size, random_state=1)
-            # create a decision tree classifier
-            self._clf = DecisionTreeClassifier(
-                criterion='entropy', 
-                random_state=100, 
-                max_depth=3, 
-                min_samples_split=5
-            )
-            # train and fit decision tree classifer
-            self._clf = self._clf.fit(self._X_train, self._y_train)
-        except Exception as e:
-            self._status['code'] = 2
-            self._status['msg'] = f'AN ERROR OCCURRED DURING THE TRAINING: {e}'
-        
-        # time.sleep(10000) # Async Testing
-        
-        self._status['code'] = 0
-        self._status['msg'] = 'TRAINING COMPLETED'
+    def train(self, criterion: str = 'entropy', max_depth: int = 3, min_samples_split: int = 5, random_state: int = 3, ccp_alpha: int = 3) -> None:
+        # create a decision tree classifier
+        self._tree = DecisionTreeClassifier(
+            criterion=criterion,
+            max_depth = max_depth,
+            min_samples_split = min_samples_split,
+            random_state = random_state,
+            ccp_alpha = ccp_alpha
+        )
+        # train and fit decision tree classifer
+        self._tree = self._tree.fit(self._X_train, self._y_train)
+
+    def train_params(self):
+        return self._tree.get_params()
 
     def predict(self):
         # train not started/finished
-        if self._status['code'] != 0:
+        if not self._tree:
             return None
         # no prediction yet, calculate one
         if not self._predict_data:
-            dtree_y_pred = self._clf.predict(self._X_test)
+            dtree_y_pred = self._tree.predict(self._X_test)
             accuracy = metrics.accuracy_score(self._y_test, dtree_y_pred)
-            precision = metrics.precision_score(self._y_test, dtree_y_pred, average='weighted')
+            precision = metrics.precision_score(self._y_test, dtree_y_pred, average='weighted', zero_division=0)
             recall = metrics.recall_score(self._y_test, dtree_y_pred, average='weighted')
             f1_score = metrics.f1_score(self._y_test, dtree_y_pred, average='weighted')
             # update predition data
@@ -76,12 +61,19 @@ class DecisionTreeTrainer:
             }        
         return self._predict_data
 
+    def tree_number_of_nodes(self):
+        # train not started/finished
+        if not self._tree:
+            return None
+        return self._tree.tree_.node_count
+
     def tree_structure(self):
         # train not started/finished
-        if self._status['code'] != 0:
+        if not self._tree:
             return None
-        tree_ = self._clf.tree_
-        feature_names = self._clf.feature_names_in_
+        tree_ = self._tree.tree_
+        feature_names = self._tree.feature_names_in_
+        # feature_names = getattr(self._tree, 'feature_names_in_', [f'feature_{i}' for i in range(tree_.n_features)])
 
         def dfs(node):
             # get necessary value
@@ -116,9 +108,12 @@ class DecisionTreeTrainer:
     
     def tree_img(self, length, width, dpi):
         # train not started/finished
-        if self._status['code'] != 0:
+        if not self._tree:
             return None
-        feature_names = self._clf.feature_names_in_
+        feature_names = self._tree.feature_names_in_
+        # tree_ = self._tree.tree_
+        # feature_names = getattr(self._tree, 'feature_names_in_', [f'feature_{i}' for i in range(tree_.n_features)])
+
         if not length:
             length = 12
         if not width:
@@ -129,7 +124,7 @@ class DecisionTreeTrainer:
         img_io = BytesIO() # create the in-memory bytes buffer
         # plot the decision tree
         plt.figure(figsize=(length, width))
-        tree.plot_tree(self._clf, filled=True, feature_names=feature_names)
+        tree.plot_tree(self._tree, filled=True, feature_names=feature_names)
         # save the plot to the in-memory buffer, not to a file
         plt.savefig(img_io, format='png', dpi=dpi)
         plt.close()  # close the plot to free memory
---
+++ src/api/decisionTreeCandidateGenerator.py
@@ -0,0 +1,117 @@
+from decisionTreeTrainer import decisionTreeTrainer
+import random
+
+
+class decisionTreeCandidateGenerator:
+    def __init__(self, X_train, y_train, X_test, y_test, config):
+        self.X_train = X_train
+        self.y_train = y_train
+        self.X_test = X_test
+        self.y_test = y_test
+        self.config = config
+        self.num_candidates = self.config.total_samples
+        self._candidates = None
+
+        self._status = {
+            'code' : -1,
+            'msg' : 'NOT TRAINED',
+            'total_number_of_samples' : self.num_candidates,
+            'number_of_samples_trained' : 0
+        }
+
+    def _sample_feature_subset(self, subset_size):
+        total_features = self.X_train.shape[1]
+        return random.sample(range(total_features), subset_size)
+    
+    def status(self):
+        return self._status
+    
+    def check_tree_status(self, tree_id):
+        if not self._candidates:
+            return (403, 'START TRAINING FIRST')
+        if not tree_id:
+            return (403, 'TREE ID IS REQUIRED')
+        if tree_id not in self._candidates:
+            return (403, f'FAIL TO FIND THE TREE ID: {tree_id}')
+        return (200, 'OK')
+
+    def train(self):
+        self._status['code'] = 1
+        self._status['msg'] = 'TRAIN START'
+        candidates = {}
+        total_features = self.X_train.shape[1]
+        for i in range(self.num_candidates):
+            params = self.config.sample_parameters(total_features)
+            feature_subset = self._sample_feature_subset(params['nr_of_nodes'])
+            tree = decisionTreeTrainer(tree_id = i + 1, 
+                                        X_train=self.X_train, 
+                                        y_train=self.y_train, 
+                                        X_test=self.X_test,
+                                        y_test=self.y_test,
+                                        feature_subset=feature_subset
+                                        )
+            tree.train(
+                criterion=params['criterion'],
+                max_depth=params['max_depth'],
+                min_samples_split=params['min_samples_split'],
+                random_state=params['random_state'],
+                ccp_alpha=params['ccp_alpha']
+                )
+            # save as dict, key = tree id
+            candidates[i] = (tree, feature_subset)
+            candidates[i] = {
+                'tree_object' : tree,
+                'feature_subset' : feature_subset
+            }
+            self._status['number_of_samples_trained'] = i + 1  # keep update current status
+        
+        self._status['code'] = 0
+        self._status['msg'] = 'TRAIN COMPLETED'
+        self._candidates = candidates
+
+    def trees_info(self, is_grouped_by_nodes: False):
+        if not self._candidates:
+            return (403, 'START TRAINING FIRST')
+        candidate_info = []
+        for t in self._candidates.values():
+            tree = t['tree_object']
+            tree_info = {
+                'tree_id': tree.id(),
+                'params': tree.train_params(),
+                'predicted': tree.predict(),
+                'number_of_nodes': tree.tree_number_of_nodes()
+            }
+            candidate_info.append(tree_info)
+        
+        def group_candidates_by_nodes():
+            grouped = {}
+            for candidate in candidate_info:
+                num_nodes = candidate['number_of_nodes']
+                if num_nodes not in grouped:
+                    grouped[num_nodes] = []
+                grouped[num_nodes].append(candidate)
+            grouped = {k: grouped[k] for k in sorted(grouped)} # Sort the dic based on key
+            return grouped
+        
+        output = {
+            'total_candidates_before_pruning': len(candidate_info),
+            'total_candidates_after_pruning': len([info for info in candidate_info if info['number_of_nodes'] > 1]),
+            'candidates': candidate_info
+        }
+        if is_grouped_by_nodes:
+            output['candidates'] = group_candidates_by_nodes()
+        return (200, output)
+
+    def tree_structure(self, tree_id):
+        ck = self.check_tree_status(tree_id=tree_id)
+        if ck[0] != 200:
+            return ck
+        return (200, self._candidates[tree_id]['tree_object'].tree_structure())
+    
+    def tree_image(self, tree_id, length, width, dpi):
+        # ck = self.check_tree_status(tree_id=tree_id)
+        # if ck[0] != 'OK':
+        #     return ck
+        return self._candidates[tree_id]['tree_object'].tree_img(length, width, dpi)
+
+    

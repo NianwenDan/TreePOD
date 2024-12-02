13f56468573125666e76359fb2946e85a7419d06
Nianwen Dan <dannianwen@gmail.com>
2024-12-02 08:41:57 -0700

    feat: api can return each tree confusion matrix and structure

--- src/app/tree/routes.py
+++ src/app/tree/routes.py
@@ -5,8 +5,44 @@ tree_bp = Blueprint('tree', __name__)
 
 @tree_bp.route('/structure', methods=['GET'])
 def structure():
-    # Simulated tree structure
-    return jsonify({'structure': 'Tree structure details'})
+    uuid4 = request.cookies.get('uuid')
+    # cannot find user
+    if uuid4 and uuid4 not in db:
+        return make_response(jsonify({'code' : 404, 'userId': None, 'msg' : 'NO USER FOUND, CREATE A USER FIRST'}), 404)
+    user_config = db[uuid4]['userConfig']
+    # user config either not set or not pass check
+    if not user_config.is_config_valid():
+        return make_response(jsonify({'code' : 400, 'userId': uuid4, 'msg' : 'USER CONFIG NOT SET OR INVALID'}), 400)
+    
+    # require tree id
+    tree_id = request.args.get('treeId', type = int)
+
+    if not tree_id:
+        data['code'] = 403
+        data['msg'] = 'MISSING TREE ID'
+        return make_response(jsonify({'code' : 400, 'userId': uuid4, 'msg' : 'MISSING TREE ID'}), 400)
+
+    generator = db[uuid4]['decisionTreeCandiateGenerator']
+    # generator not created yet
+    if not generator:
+        data = {
+            'code' : 200,
+            'data': {
+                'code' : -1,
+                'msg' : 'NO GENERATOR / NOT TRAINED'
+            },
+            'userId': uuid4,
+            'msg' : 'GENERATOR WILL BE CREATED WHEN /START-TRAIN CALLED'
+        }
+        return jsonify(data)
+    data = {
+        'code' : 200,
+        'data': generator.tree_structure(tree_id=tree_id),
+        'userId': uuid4,
+        'msg' : 'OK'
+    }
+
+    return jsonify(data)
 
 @tree_bp.route('/hierarchy', methods=['GET'])
 def hierarchy():
@@ -42,5 +78,41 @@ def hierarchy():
 
 @tree_bp.route('/confusion-matrix', methods=['GET'])
 def confusion_matrix():
-    # Simulated confusion matrix
-    return jsonify({'confusion_matrix': [[50, 10], [5, 35]]})
+    uuid4 = request.cookies.get('uuid')
+    # cannot find user
+    if uuid4 and uuid4 not in db:
+        return make_response(jsonify({'code' : 404, 'userId': None, 'msg' : 'NO USER FOUND, CREATE A USER FIRST'}), 404)
+    user_config = db[uuid4]['userConfig']
+    # user config either not set or not pass check
+    if not user_config.is_config_valid():
+        return make_response(jsonify({'code' : 400, 'userId': uuid4, 'msg' : 'USER CONFIG NOT SET OR INVALID'}), 400)
+    
+    # require tree id
+    tree_id = request.args.get('treeId', type = int)
+
+    if not tree_id:
+        data['code'] = 403
+        data['msg'] = 'MISSING TREE ID'
+        return make_response(jsonify({'code' : 400, 'userId': uuid4, 'msg' : 'MISSING TREE ID'}), 400)
+
+    generator = db[uuid4]['decisionTreeCandiateGenerator']
+    # generator not created yet
+    if not generator:
+        data = {
+            'code' : 200,
+            'data': {
+                'code' : -1,
+                'msg' : 'NO GENERATOR / NOT TRAINED'
+            },
+            'userId': uuid4,
+            'msg' : 'GENERATOR WILL BE CREATED WHEN /START-TRAIN CALLED'
+        }
+        return jsonify(data)
+    data = {
+        'code' : 200,
+        'data': generator.get_confusion_matrix(tree_id=tree_id),
+        'userId': uuid4,
+        'msg' : 'OK'
+    }
+
+    return jsonify(data)

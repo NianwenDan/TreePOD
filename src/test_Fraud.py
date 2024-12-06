eb589ea81cc60c95c65bd09466bb0462bb501eb9
Yihe Zhuo <86751915+RubyCheuk@users.noreply.github.com>
2024-12-06 10:16:43 -0700

    New dataset processing. (#57)

--- src/test_Fraud.py
+++ src/test_Fraud.py
@@ -21,9 +21,9 @@ def check_and_load_data(path):
     if all_files_exist:
         # Load the CSV files as DataFrames
         X_train = pd.read_csv(os.path.join(path, "Fraud_X_train.csv"))
-        y_train = pd.read_csv(os.path.join(path, "Fraud_y_train.csv"))['isFraud']
+        y_train = pd.read_csv(os.path.join(path, "Fraud_y_train.csv"))['FraudFound_P']
         X_test = pd.read_csv(os.path.join(path, "Fraud_X_test.csv"))
-        y_test = pd.read_csv(os.path.join(path, "Fraud_y_test.csv"))['isFraud']
+        y_test = pd.read_csv(os.path.join(path, "Fraud_y_test.csv"))['FraudFound_P']
         return X_train, y_train, X_test, y_test
 
 def dataProcessing_Fraud(df, categorical_columns):
@@ -36,6 +36,11 @@ def dataProcessing_Fraud(df, categorical_columns):
         df[col] = df[col].str.strip()
         df[col] = df[col].fillna(df[col].mode()[0])
 
+    df['MonthClaimed'] = df['MonthClaimed'].map({'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6, 'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12, 0: 0})
+    df['Month'] = df['Month'].map({'Jan': 1, 'Feb': 2, 'Mar': 3, 'Apr': 4, 'May': 5, 'Jun': 6, 'Jul': 7, 'Aug': 8, 'Sep': 9, 'Oct': 10, 'Nov': 11, 'Dec': 12})
+    df['DayOfWeek'] = df['DayOfWeek'].map({'Monday': 1, 'Tuesday': 2, 'Wednesday': 3, 'Thursday': 4, 'Friday': 5, 'Saturday': 6, 'Sunday': 7})
+    df['DayOfWeekClaimed'] = df['DayOfWeekClaimed'].map({'Monday': 1, 'Tuesday': 2, 'Wednesday': 3, 'Thursday': 4, 'Friday': 5, 'Saturday': 6, 'Sunday': 7, 0: 0})
+
     # Apply one-hot encoding to categorical columns
     categorical_df = pd.get_dummies(df[[col for col in categorical_columns]])  # Exclude the target column
 
@@ -44,14 +49,14 @@ def dataProcessing_Fraud(df, categorical_columns):
     #X.columns = [col[:-1] if col.endswith('.') else col for col in X.columns]
 
     # Step 4: Encode the target variable   
-    y = df['isFraud']
+    y = df['FraudFound_P']
     return X, y
 
-def mainDataProcessing_Fraud(path, dataProcessing_UCI, categorical_columns):
+def mainDataProcessing_Fraud(path, dataProcessing_fraud, categorical_columns):
     # Drop the high correlated column "relationship"
-    combined_df = pd.read_csv(os.path.join(path, 'onlinefraud.csv')).drop(['nameOrig', 'nameDest'], axis=1)
-
-    X, y = dataProcessing_Fraud(combined_df, categorical_columns)
+    df = pd.read_csv(os.path.join(path, 'fraud_oracle.csv')).drop(['PolicyNumber', 'Age'], axis=1)
+    
+    X, y = dataProcessing_Fraud(df, categorical_columns)
 
     # Split back into training and testing sets
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=50)
@@ -64,7 +69,9 @@ def mainDataProcessing_Fraud(path, dataProcessing_UCI, categorical_columns):
     return X_train, y_train, X_test, y_test
 
 # Check and load data
-categorical_columns = ['type']
+categorical_columns = ['Make', 'AccidentArea', 'Sex', 'MaritalStatus', 'Fault', 'PolicyType', 'VehicleCategory', 'VehiclePrice', 
+       'Days_Policy_Accident', 'Days_Policy_Claim','PastNumberOfClaims', 'AgeOfVehicle', 'AgeOfPolicyHolder',
+       'PoliceReportFiled', 'WitnessPresent', 'AgentType', 'NumberOfSuppliments', 'AddressChange_Claim', 'NumberOfCars', 'BasePolicy']
 path = 'datasets/'
 
 result = check_and_load_data(path)
@@ -80,15 +87,19 @@ print ('column_mapping: ', column_mapping)
 
 # User-defined config
 # user_config = decisionTreeConfig(max_depth_range=range(1, 7), random_state_range=range(50, 51), total_samples=1000)
-user_config = decisionTreeConfig()
-user_config.set_parameters(
-    total_samples=10
-)
-user_config.fill_undefined_parameters_randomly()
-print(user_config.get_rand_param_based_on_user_config())
+decision_Tree_Config = decisionTreeConfig()
+decision_Tree_Config.set_parameters(user_config_json={
+        # "feature-set": ['1', '2', '3'], # Not Implemented Yet
+        "max-depth-range": [1, 8],
+        "min-leaf-size": 20,
+        "pruning": False,
+        # "round-to-significant-digit": 3, # Not Implemented Yet
+        "selection-criterion": ['gini', 'entropy', 'log_loss'],
+        "stochastic-samples": 1000
+})
 
 # Generate decision tree candidates
-generator = decisionTreeCandidateGenerator(X_train, y_train, X_test, y_test, column_mapping, config=user_config)
+generator = decisionTreeCandidateGenerator(X_train, y_train, X_test, y_test, column_mapping, config=decision_Tree_Config)
 generator.train()
 
 # It's ok just override the original file, too many old files make it harder to understand what are they doing.
@@ -100,8 +111,6 @@ output = {
     "msg": "OK",
     "userId": "f6a411d5-988d-4c21-9700-0418482b6fac"
 }
-print (output)
-'''
 with open('../example/api/model/trees.json', 'w') as json_file:
     json.dump(output, json_file, indent=4)
 
@@ -121,15 +130,18 @@ with open('../example/api/tree/hierarchy_data.json', 'w') as json_file:
     json.dump(hierarchy_data, json_file, indent=4)
 
 # Output the last Pareto-optimal tree
-tree_output = generator.tree_structure(int(output['data']['pareto_front']['f1_score__number_of_nodes'][-1]))
-tree_output = {
-    "code": 200,
-    "data": tree_output,
-    "msg": "OK",
-    "userId": "f6a411d5-988d-4c21-9700-0418482b6fac"
-}
-with open('../example/api/tree/structure.json', 'w') as json_file:
-    json.dump(tree_output, json_file, indent=4)
+for i in range(0, len(output['data']['pareto_front']['f1_score__number_of_nodes'])):
+    id = output['data']['pareto_front']['f1_score__number_of_nodes'][i]
+    tree_structure = generator.tree_structure(int(id))
+    tree_output = {
+        "code": 200,
+        "data": tree_structure,
+        "msg": "OK",
+        "userId": "f6a411d5-988d-4c21-9700-0418482b6fac"
+    }
+    with open(f'../example/api/tree/structure.{id}.json', 'w') as json_file:
+        json.dump(tree_output, json_file, indent=4)
+
 
 # Output the last Pareto-optimal tree confusion matrix
 tree_confusion_matrix = generator.get_confusion_matrix(int(output['data']['pareto_front']['f1_score__number_of_nodes'][-1]))
@@ -140,4 +152,4 @@ tree_confusion_matrix = {
     "userId": "f6a411d5-988d-4c21-9700-0418482b6fac"
 }
 with open('../example/api/tree/confusion-matrix.json', 'w') as json_file:
-    json.dump(tree_confusion_matrix, json_file, indent=4)'''
+    json.dump(tree_confusion_matrix, json_file, indent=4)
